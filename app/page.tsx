a0958ef2e1e545d81fab50ce31482c1a1626c881
Kamsnyc <kamel.singh1999@gmail.com>
2023-09-25 20:26:05 -0400

    added hero and fixed deployment issues

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <main className="">
       {/* HERO SECTION */}
-      <section className="h-[600px] relative text-center mx-[10px] md:mx-[120px] xl:mx-[200px] 2xl:mx-[320px]">
+      <section className="h-[300px] lg:h-[600px] relative text-center md:text-left mx-[10px] md:mx-[120px] xl:mx-[200px] 2xl:mx-[320px]">
         <Hero />
 
         {/* GLOBE IMAGE */}
-        <div className="absolute top-[-20%] right-[-40%] 2xl:right-[-20%] z-[-1]">
+        <div className="absolute top-[-15%] right-[-40%] 2xl:right-[-20%] z-[-1]">
           <Image className="hidden md:flex opacity-40" src="/Globe.gif" width={800} height={600} alt="test" />
         </div>
       </section>
@@ -20,34 +20,34 @@ export default function Home() {
      
       {/* ABOUT SECTION */}
       <section id="about" className="w-full h-screen flex justify-center items-center">
-        <div className="container mx-auto p-2 grid grid-cols-2 gap-4">
+        <div className="container mx-auto h-full p-2 flex-col lg:grid grid-cols-2 gap-4">
     
           <div className="col-span-1 flex-col ">
             <h1 className='text-center'>About Me</h1>
             <p className='px-4 py-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsum consequuntur dolor odit, officiis mollitia nulla sunt commodi, vitae illo eos cupiditate assumenda amet provident natus ea explicabo fuga facilis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptas ullam culpa itaque odio quae perspiciatis eius incidunt tempora rerum ex laudantium, et, eligendi vero quibusdam obcaecati magnam nihil quos?
             </p>
 
-            <div id='skills' className="flex justify-between items-center gap-20 w-full h-[50px] bg-red-50 rounded-2xl px-8 mt-20 ">
+            <div id='skills' className="flex justify-between items-center gap-10 w-full h-[50px] bg-red-50 rounded-2xl px-8 mt-10 lg:mt-20 ">
 
-              <div className="text-black">
+              <div className="text-black ">
                 Icon</div>
 
-                <div className="text-black">
+                <div className="text-black ">
                 Icon</div>
 
-                <div className="text-black">
+                <div className="text-black ">
                 Icon</div>
 
-                <div className="text-black">
+                <div className="text-black ">
                 Icon</div>
 
-                <div className="text-black">
+                <div className="text-black ">
                 Icon</div>
 
             </div>
           </div>
 
-          <div className="col-span-1 flex justify-center">
+          <div className="mt-20 col-span-1 flex justify-center">
             <Image src="/your-image.jpg" width={600} height={600} alt="Your Picture" />
           </div>
 
